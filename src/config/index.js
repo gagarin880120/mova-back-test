6b18a462c17ef59a5851a7b097f73fa20e6831da
Andrei <gagarin880120@gmail.com>
2021-01-08 19:49:11 +0300

    fix: some minor changes

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT,
-  MOB_URL:
-    'mongodb+srv://admin:G8G2Bfs5cBepEdr@cluster-mova-data-base.bnb39.mongodb.net/mova-data-base-user?retryWrites=true&w=majority',
+  MOB_URL: process.env.MOB_URL,
   SECRET_JWT_KEY: process.env.SECRET_JWT_KEY
 };
